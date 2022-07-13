c1234fbf9de7eb37ba9928ab7c92d7b90755d8f1
Max Vogel <max-v@berkeley.edu>
2022-07-13 15:54:35 -0700

    init mpv

--- scripts/tp-fix-brightness.sh
+++ scripts/tp-fix-brightness.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Change the line below according to your hardware
 BRIGHTNESS_FILE="/var/lib/systemd/backlight/pci-0000:07:00.0:backlight:amdgpu_bl0"
 BRIGHTNESS=$(cat "$BRIGHTNESS_FILE")
 BRIGHTNESS=$(($BRIGHTNESS*255/65535))
